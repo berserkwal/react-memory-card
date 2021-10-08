61e64158c6f470c1a3f25c2e94c822c835596494
Vikas Kharkwal <kharkwalvikas@gmail.com>
2021-10-08 14:00:08 +0530

    removal of redundant code to test concept

--- src/components/Gameboard.js
+++ src/components/Gameboard.js
@@ -2,21 +2,15 @@ import Card from './Card';
 import Loading from './Loading';
 
 function Gameboard(props) {
-  const arr = [
-    {
-      name: 'Vue',
-      img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png',
-    },
-    {
-      name: 'Vue',
-      img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png',
-    },
-  ];
-  const cards = props.pokeData.map((item, index) => (
-    <Card id={item.id} name={item.name} key={item.id} types={item.types} />
+  const cards = props.pokeData.map((item) => (
+    <Card
+      id={item.id}
+      name={item.name}
+      key={item.id}
+      types={item.types}
+      selectCard={props.selectCard}
+    />
   ));
-
-  // const content = props.isLoading ? <Loading /> : cards;
 
   return (
     <div className='gameboard'>
