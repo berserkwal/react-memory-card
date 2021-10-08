c32ddacb328a74176083741fd17a2817040dea5d
Vikas Kharkwal <kharkwalvikas@gmail.com>
2021-10-08 14:00:33 +0530

    added a level denoter and more

--- src/components/Scoreboard.js
+++ src/components/Scoreboard.js
@@ -1,8 +1,11 @@
 function Scoreboard(props) {
   return (
     <div className='board-wrapper'>
-      <h3 className='board-current'>Current Score: {props.currentScore}</h3>
-      <h3 className='board-best'>Best Score: {props.bestScore}</h3>
+      <h3 className='board-level'>Level: {props.currentLevel}</h3>
+      <div className='board-score'>
+        <h3 className='score-current'>Current Score: {props.currentScore}</h3>
+        <h3 className='score-best'>Best Score: {props.bestScore}</h3>
+      </div>
     </div>
   );
 }
